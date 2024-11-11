296de058d92415696c37b305c9246a1066785f32
MusaShoeb <mshoeb@umich.edu>
2024-11-10 22:28:17 -0500

    Updated Events

--- src/Simulation.ts
+++ src/Simulation.ts
@@ -1,217 +1,5 @@
-enum JobType {
-  Small = "SMALL",
-  Medium = "MEDIUM",
-  Large = "LARGE",
-}
-
-//Setup Event Types and Queues
-
-enum AlgorithmType {
-  FirstFit = "FF",
-  NextFit = "NF",
-  BestFit = "BF",
-  WorstFit = "WF",
-}
-
-enum EventType {
-  Arrival = "Arrival",
-  Termination = "Termination",
-  HeapAllocation = "HA",
-  HeapTermination = "HT",
-}
-
-interface Event {
-  type: EventType;
-  job: Job;
-  arrivalTime: number;
-  heapElement: HeapElement | null;
-}
-
-interface HeapElement {
-  id: number;
-  heapMemorySize: number;
-  memoryUnits: number;
-  heapLifeTime: number | null;
-  location: number | null;
-}
-
-interface Job {
-  type: JobType;
-  runTime: number;
-  codeSize: number;
-  stackSize: number;
-  heapElements: Array<HeapElement>;
-  arrivalTime: number;
-  codeLocation: number | null;
-  stackLocation: number | null;
-}
-
-interface MemoryBlock {
-  location: number;
-  size: number;
-  free: boolean;
-}
-
-class MemoryPool {
-  memoryBlocks: Array<MemoryBlock> = [];
-  type: AlgorithmType;
-  totalMemoryUnits: number;
-  freeMemoryUnits: number;
-  allocatedMemoryUnits: number = 0;
-  totalJobsProcessed: number = 0;
-  nextFitPointer: number = 0;
-  internalFragmentation: number = 0; //Tracks wasted space within allocated memory units.
-  externalFragmentation: number = 0; //Tracks scattered free blocks that cannot satisfy a request.
-
-  constructor(type: AlgorithmType, totalSize: number) {
-    this.type = type;
-    this.totalMemoryUnits = totalSize;
-    this.freeMemoryUnits = totalSize;
-  }
-
-  public mallocFF(size: number) {
-    const startingPosition = 0;
-    let freeUnitCount = 0;
-
-    for(let i = 0; i < this.memoryBlocks.length; i++) {
-      const memoryBlock = this.memoryBlocks[startingPosition];
-      if (memoryBlock.free) {
-        freeUnitCount += memoryBlock.size;
-        if (freeUnitCount === size) {
-          for (let i = 0; i < size; i++) {
-            this.memoryBlocks[memoryBlock.location + i].free = false; // Use job ID here in a real scenario
-          }
-          return memoryBlock.location; 
-        }
-      } else {
-        freeUnitCount = 0;
-      }
-    }
-
-    //Nothing found;
-    return null;
-  }
-
-  public mallocNF(size: number) {
-    const startingPosition = this.nextFitPointer;
-    let freeUnitCount = 0;
-
-    for(let i = 0; i < this.memoryBlocks.length; i++) {
-      const memoryBlock = this.memoryBlocks[startingPosition];
-      if (memoryBlock.free) {
-        freeUnitCount += memoryBlock.size;
-        if (freeUnitCount === size) {
-          for (let i = 0; i < size; i++) {
-            this.memoryBlocks[memoryBlock.location + i].free = false; 
-          }
-          this.nextFitPointer = memoryBlock.location;
-          return memoryBlock.location; 
-        }
-      } else {
-        freeUnitCount = 0;
-      }
-    }
-
-    //Nothing found;
-    return null;
-  }
-
-  public mallocWF(size: number) {
-    const freeBlocksMap: Map<number, number> = new Map();
-
-    for (let i = 0; i < this.memoryBlocks.length; i++) {
-      const currBlock = this.memoryBlocks[i];
-      if (currBlock.free) {
-        let startLocation = i;
-        let blockSize = 0;
-
-        while (i < this.memoryBlocks.length && this.memoryBlocks[i].free) {
-          blockSize += this.memoryBlocks[i].size;
-          i++;
-        }
-
-        freeBlocksMap.set(startLocation, blockSize);
-      }
-    }
-
-    let largestFreeBlockStart: number | null = null;
-    let largestBlockSize = 0;
-
-    for (const [startLocation, blockSize] of freeBlocksMap.entries()) {
-      if (blockSize >= size && blockSize > largestBlockSize) {
-        largestFreeBlockStart = startLocation;
-        largestBlockSize = blockSize;
-      }
-    }
-
-    if (largestFreeBlockStart === null) {
-      //Log error
-      return null;
-    }
-
-    let remainingSize = size;
-    for (let i = largestFreeBlockStart; remainingSize > 0 && i < this.memoryBlocks.length; i++) {
-      if (this.memoryBlocks[i].free) {
-        this.memoryBlocks[i].free = false;
-        remainingSize -= this.memoryBlocks[i].size;
-      }
-    }
-
-    this.freeMemoryUnits -= size;
-    this.allocatedMemoryUnits += size;
-
-    return largestFreeBlockStart;
-
-  }
-
-  public mallocBF(size: number) {
-    const freeBlocksMap: Map<number, number> = new Map();
-
-    for (let i = 0; i < this.memoryBlocks.length; i++) {
-      const currBlock = this.memoryBlocks[i];
-      if (currBlock.free) {
-        let startLocation = i;
-        let blockSize = 0;
-
-        while (i < this.memoryBlocks.length && this.memoryBlocks[i].free) {
-          blockSize += this.memoryBlocks[i].size;
-          i++;
-        }
-
-        freeBlocksMap.set(startLocation, blockSize);
-      }
-    }
-
-    let smallestFreeBlockStart: number | null = null;
-    let smallestBlockSize = 0;
-
-    for (const [startLocation, blockSize] of freeBlocksMap.entries()) {
-      if (blockSize >= size && blockSize < smallestBlockSize) {
-        smallestFreeBlockStart = startLocation;
-        smallestBlockSize = blockSize;
-      }
-    }
-
-    if (smallestFreeBlockStart === null) {
-      //Log error
-      return null;
-    }
-
-    let remainingSize = size;
-    for (let i = smallestFreeBlockStart; remainingSize > 0 && i < this.memoryBlocks.length; i++) {
-      if (this.memoryBlocks[i].free) {
-        this.memoryBlocks[i].free = false;
-        remainingSize -= this.memoryBlocks[i].size;
-      }
-    }
-
-    this.freeMemoryUnits -= size;
-    this.allocatedMemoryUnits += size;
-
-    return smallestFreeBlockStart;
-
-  }
-}
+import {JobType, AlgorithmType, EventType, Event, HeapElement, Job, MemoryBlock, MemoryPool, HeapStatus} from './Objects'; 
+import { mallocFF, mallocBF, mallocNF, mallocWF } from './MemAlgorithms';
 
 class MemorySimulation {
   smallJobPercentage: number;
@@ -244,7 +32,7 @@ class MemorySimulation {
   }
 
   public GenerateRandomNum(min: number, max: number): number {
-    return Math.floor(Math.random() * max) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   public HeapGenerator(currentJob: Job, heapObjects: number) {
@@ -258,7 +46,7 @@ class MemorySimulation {
         memoryUnits: heapUnitSize,
         heapLifeTime: null,
         location: null,
-      };
+        status: HeapStatus.Start, };
       currentJob.heapElements.push(heapObject);
     }
   }
@@ -269,7 +57,7 @@ class MemorySimulation {
     stack: number,
     runTime: number,
     arrivalTime: number,
-    heapElements: Array<HeapElement>
+    
   ): Job {
     const newJob: Job = {
       type: type,
@@ -277,7 +65,7 @@ class MemorySimulation {
       stackSize: stack,
       runTime: runTime,
       arrivalTime: arrivalTime,
-      heapElements: heapElements,
+      heapElements: [],
       codeLocation: null,
       stackLocation: null,
     };
@@ -301,7 +89,7 @@ class MemorySimulation {
   public fillSimulationJobs(): void {
     let i: number;
     let arrival: number = 0;
-    for (i = 0; i < 1; i++) {
+    while (arrival < 10) {
       let type: string = this.GenerateRandJobType();
       switch (type) {
         case JobType.Small: {
@@ -310,10 +98,9 @@ class MemorySimulation {
             this.GenerateRandomNum(40, 80),
             this.GenerateRandomNum(20, 40),
             this.GenerateRandomNum(4, 6),
-            arrival,
-            []
+            arrival
           );
-          this.HeapGenerator(newJob, newJob.runTime * 50);
+          this.HeapGenerator(newJob, newJob.runTime * 1);
           this.jobsQueue.push(newJob);
           this.logJob(newJob);
           break;
@@ -324,10 +111,9 @@ class MemorySimulation {
             this.GenerateRandomNum(60, 120),
             this.GenerateRandomNum(40, 80),
             this.GenerateRandomNum(9, 11),
-            arrival,
-            []
+            arrival
           );
-          this.HeapGenerator(newJob, newJob.runTime * 100);
+          this.HeapGenerator(newJob, newJob.runTime * 1);
           this.jobsQueue.push(newJob);
           this.logJob(newJob);
           break;
@@ -338,10 +124,9 @@ class MemorySimulation {
             this.GenerateRandomNum(120, 220),
             this.GenerateRandomNum(60, 120),
             this.GenerateRandomNum(24, 26),
-            arrival,
-            []
+            arrival
           );
-          this.HeapGenerator(newJob, newJob.runTime * 250);
+          this.HeapGenerator(newJob, newJob.runTime * 1);
           this.jobsQueue.push(newJob);
           this.logJob(newJob);
           break;
@@ -365,35 +150,44 @@ class MemorySimulation {
   }
 
   public fillEventQueue(): void {
-    this.jobsQueue.forEach((job, index) => {
+    this.jobsQueue.forEach((job) => {
+      // Create Arrival Event
       this.createEvent(EventType.Arrival, job.arrivalTime, job, null);
-      
-      var heapArrival = job.arrivalTime;
-      var heapAllocatedBatchSize = 0;
-      
+  
+      // Create Termination Event (job's run time)
+      this.createEvent(EventType.Termination, job.arrivalTime + job.runTime, job, null);
+  
+      let heapArrival = job.arrivalTime;
+      let heapAllocatedBatchSize = 0;
+  
       job.heapElements.forEach((heapEl) => {
-        var heapTermination: number = this.GenerateRandomNum(
-          heapArrival - (heapArrival - 1),
-          heapArrival
-        );
-        this.createEvent(EventType.HeapAllocation, heapArrival, job, heapEl);
-        this.createEvent(EventType.HeapTermination, heapTermination, job, heapEl);
-        heapAllocatedBatchSize++;
+        
         if (heapAllocatedBatchSize === 50) {
-          heapArrival++;
+          heapArrival++; 
           heapAllocatedBatchSize = 0;
         }
+  
+        // Schedule Heap Allocation Event
+        this.createEvent(EventType.HeapAllocation, heapArrival, job, heapEl);
+  
+       
+        const heapTerminationTime = this.GenerateRandomNum(
+          heapArrival + 1,  
+          job.arrivalTime + job.runTime 
+        );
+  
+       
+        this.createEvent(EventType.HeapTermination, heapTerminationTime, job, heapEl);
+  
+        
+        heapAllocatedBatchSize++;
       });
-      this.createEvent(
-        EventType.Termination,
-        job.arrivalTime + job.runTime,
-        job,
-        null
-      );
     });
-      this.eventsQueue.sort((eventA, eventB) => eventA.arrivalTime - eventB.arrivalTime);
+  
+   
+    this.eventsQueue.sort((eventA, eventB) => eventA.arrivalTime - eventB.arrivalTime);
   }
-
+  
   public startSimulation(): void {
     this.fillSimulationJobs();
     this.fillEventQueue();
